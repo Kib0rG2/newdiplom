91280e2b45626b24451a26c496fbf94e7734df99
Kib0rG2 <evgenijgorskij6@gmail.com>
2024-06-22 12:05:51 +0300

    Четвёртое  пришествие

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   var currentIndex = 0;
   var slides = $('.slider-image');
   var totalSlides = slides.length;
-  var interval = setInterval(nextSlide, 2000); // Интервал в миллисекундах (5000 = 5 секунд)
+  var slideWidth = slides.eq(0).width(); // Ширина одного слайда
+  var interval = setInterval(nextSlide, 2000); // Интервал в миллисекундах (2000 = 2 секунды)
 
   function showSlide(index) {
     if (index < 0) {
@@ -10,7 +11,7 @@ $(document).ready(function(){
     } else if (index >= totalSlides) {
       index = 0;
     }
-    $('#slider-images').css('transform', 'translateX(' + (-index * 500) + 'px)');
+    $('#slider-images').css('transform', 'translateX(' + (-index * slideWidth) + 'px)');
     currentIndex = index;
   }
 
@@ -27,4 +28,17 @@ $(document).ready(function(){
     clearInterval(interval); // Сбросить интервал при клике на следующий слайд
     showSlide(currentIndex + 1);
   });
+
+  // Адаптация при изменении размера окна
+  $(window).on('resize', function() {
+    slideWidth = slides.eq(0).width(); // Обновляем ширину слайда при изменении размера окна
+    showSlide(currentIndex); // Показываем текущий слайд с учетом новой ширины
+  });
+
+  // Отключение автоматического слайдшоу при изменении размера окна (не обязательно, но полезно)
+  $(window).on('resize', function() {
+    clearInterval(interval); // Сбросить интервал при изменении размера окна
+    interval = setInterval(nextSlide, 2000); // Перезапуск интервала с новыми настройками
+  });
+
 });
